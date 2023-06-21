8337ef078f9945b500f150f66ff2b52caa55aa6d
humphrey <ahnsv@bc.edu>
2023-06-21 17:50:58 +0900

    Create poetry-config.lua

---
+++ nvim/poetry-config.lua
@@ -0,0 +1,25 @@
+local util = require "lspconfig/util"
+local path = util.path
+
+local function get_python_path(workspace)
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+    end
+
+    -- Find and use virtualenv via poetry in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+    if match ~= "" then
+        local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
+        return path.join(venv, "bin", "python")
+    end
+
+    -- Fallback to system Python.
+    return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
+end
+
+lspconfig.pyright.setup {
+  on_init = function(client)
+        client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+  end,
+}
